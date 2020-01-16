dd5e9b296f4e8551c1532caafcb0763c9c0c40e8
Joe Hayden <eljoeyy@gmail.com>
2020-01-16 13:06:25 -0600

    basic homepage setup

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Home = () => {
-  return <div>home</div>
+const Home = ({ blogs }) => {
+  const blogList = () => {
+    if (blogs === undefined) {
+      return <div>No Blogs!</div>
+    } else {
+      return blogs.map(blog => (
+        <div>
+          <span>- </span>
+          <Link to={`/blogs/${blog._id}`}>
+            {' '}
+            {blog.title} ({blog.comments.length} comments){' '}
+          </Link>
+        </div>
+      ))
+    }
+  }
+  return (
+    <>
+      <h1>List of Blogs</h1>
+      <div>{blogList()}</div>
+    </>
+  )
 }
 
 export default Home
